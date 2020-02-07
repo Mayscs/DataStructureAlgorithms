5ddb186b8c08f10a91ea9613c830d7f8c8a632cd
Mayscs <mayank.gupta@appdirect.com>
2020-02-07 10:28:40 +0530

    DSA-1: Dynamic Array- Remove value.

--- src/main/java/DynamicArray.java
+++ src/main/java/DynamicArray.java
@@ -1,6 +1,7 @@
 import java.util.Iterator;
 
 import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
 
 @Getter
 public class DynamicArray<T> implements Iterable<T> {
@@ -34,8 +35,27 @@ public class DynamicArray<T> implements Iterable<T> {
 	}
 
 	public void remove(T value){
-			
+	int index = indexOf(value);
+	if (index == -1) throw new ValueException("Value not found");
+	else remove(index);
 	}
+
+	private int indexOf(T value) {
+		for(int i=0; i < length; i++) {
+			if (array[i] == value)
+				return i;
+		}
+		return -1;
+	}
+
+	public void remove(int rmIndex){
+		if(rmIndex < 0 && rmIndex >=length) throw new IndexOutOfBoundsException("Remove Index out of size of array.");
+		if (rmIndex == length-1) array[rmIndex]=null;
+		for(int i=rmIndex; i<length-1; i++){ ;
+			array[i]=array[i+1];
+		}
+	}
+
 	public Iterator<T> iterator() {
 		return null;
 	}
